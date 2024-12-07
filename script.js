8e406abe91655925fd83c8dc74881923f7f75461
Reyanshflas <reyanshflasplayz@gmail.com>
2024-12-07 15:30:54 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(x, y){
+    document.getElementById("para-1").innerText = x+y
+}
+
+function subtract(a, b){
+    document.getElementById("para-2").innerText = a-b
+}
+
+function multiply(r, t){
+    document.getElementById("para-3").innerText = r*t
+}
+
+function divide(g, p){
+    document.getElementById("para-4").innerText = g/p
+}
+
+function modulas(d, i){
+    document.getElementById("para-5").innerText = d%i
+}
